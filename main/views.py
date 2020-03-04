4f14cc4b2e4c4b8e300506f98005133159f30dda
Sunil Patidar <patidarsunil110@gmail.com>
2020-03-04 18:29:54 +0530

    Added Messages into views.py file but when add into html file form validation not True.

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from main.models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def homepage(request):
@@ -15,16 +16,23 @@ def homepage(request):
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+        print(form.is_valid())
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect("main:homepage")
+
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+            return render(request=request,
+                          template_name="main/register.html",
+                          context={"form": form})
 
     form = UserCreationForm
-    return render(request,
-                  "main/register.html",
-                  context={"form": form}
-                  )
+    return render(request=request,
+                  template_name="main/register.html",
+                  context={"form": form})
