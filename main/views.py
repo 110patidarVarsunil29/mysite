04f4b7fa68da238fd77133834befc99655d2f56a
Sunil Patidar <patidarsunil110@gmail.com>
2020-03-07 18:53:00 +0530

    Added category and categories pages into templates and added single slug function into views.py

--- main/views.py
+++ main/views.py
@@ -1,15 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from main.models import Tutorial
-from django.contrib.auth.forms import  AuthenticationForm
+from main.models import Tutorial, TutorialCategory, TutorialSeries
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import NewUserForm
 
 
+def single_slug(request, single_slug):
+    # first check to see if the url is in categories.
+
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request=request,
+                      template_name='main/category.html',
+                      context={"tutorial_series": matching_series, "part_ones": series_urls})
+
+
 def homepage(request):
     return render(request=request,
-                  template_name="main/home.html",
-                  context={"tutorials": Tutorial.objects.all()}
+                  template_name="main/categories.html",
+                  context={"categories": TutorialCategory.objects.all()}
                   )
 
 
