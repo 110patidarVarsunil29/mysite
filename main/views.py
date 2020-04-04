16236fbc9d1eb06cfd2666df0c9eb96de1341ec2
patidarsunil110 <patidarsunil110@gmail.com>
2020-04-05 00:38:53 +0530

    Admin, login, register and categories issues are resolved but still tutorial slug page could not resolved.

--- main/views.py
+++ main/views.py
@@ -7,6 +7,40 @@ from django.contrib import messages
 from .forms import NewUserForm
 
 
+def single_slug(request, single_slug):
+    # first check to see if the url is in categories.
+
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest(
+                "tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request=request,
+                      template_name='main/category.html',
+                      context={ "tutorial_series": matching_series, "part_ones": series_urls})
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+        tutorials_form_series = Tutorial.objects.filter(
+            tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by("tutorial_published")
+
+        this_tutorial_idx = list(tutorials_form_series).index(this_tutorial)
+
+        return render(request=request,
+                      template_name="main/tutorial.html",
+                      context={"tutorial": this_tutorial,
+                               "sidebar": tutorials_form_series,
+                               "this_tutorial_idx": this_tutorial_idx})
+
+    return HttpResponse(f"{single_slug} does not correspond to anything!")
+
+
 def homepage(request):
     return render(request=request,
                   template_name="main/categories.html",
@@ -64,38 +98,3 @@ def logout_request(request):
     logout(request)
     messages.info(request, "User Logout successfully!")
     return redirect("main:homepage")
-
-
-def single_slug(request, single_slug):
-    # first check to see if the url is in categories.
-
-    categories = [c.category_slug for c in TutorialCategory.objects.all()]
-    if single_slug in categories:
-        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
-        series_urls = {}
-
-        for m in matching_series.all():
-            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
-            series_urls[m] = part_one.tutorial_slug
-
-        return render(request=request,
-                      template_name='main/category.html',
-                      context={"part_ones": series_urls})
-    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
-    if single_slug in tutorials:
-        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
-        tutorials_form_series = Tutorial.objects.filter(tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by("tutorial_published")
-
-        this_tutorial_idx = list(tutorials_form_series).index(this_tutorial)
-
-        return render(request,
-                      "main/tutorial.html",
-                      {"tutorial": this_tutorial,
-                       "sidebar": tutorials_form_series,
-                       "this_tutorial_idx": this_tutorial})
-
-    # return HttpResponse(f"{single_slug} does not correspond to anything!")
-    # return render(request,
-    #              template_name="main/slug_error.html",
-    #              context={"single_slug": single_slug})
-
